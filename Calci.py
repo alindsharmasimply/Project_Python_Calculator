62745c8b548903e56c5e17ab1530cecf87bb3b5d
Alind Sharma <alindsharmasimply@gmail.com>
2017-09-17 18:09:04 +0530

    Added the 'Clear All' and 'Del' functionality

--- Calci.py
+++ Calci.py
@@ -4,12 +4,21 @@ import parser
 root = Tk()
 root.title('Calculator')
 #get the user input
+i = 0
 
-i=0
 def get_numbers(num):
 	global i
 	display.insert(i,num)
 	i += 1
+
+def clear_all():
+	display.delete(0,END)
+
+def one_by_one():
+	entire_string = display.get()
+	new_string = entire_string[:-1]
+	clear_all()
+	display.insert(0,new_string)
 
 #adding the input field
 display = Entry(root)
@@ -29,13 +38,13 @@ Button(root, text = "7", command = lambda: get_numbers(7)).grid(row = 4, column 
 Button(root, text = "8", command = lambda: get_numbers(8)).grid(row = 4, column = 1)
 Button(root, text = "9", command = lambda: get_numbers(9)).grid(row = 4, column = 2)
 
-Button(root, text = "AC").grid(row = 5, column =0)
+Button(root, text = "AC", command = lambda: clear_all()).grid(row = 5, column =0)
 Button(root, text = "/").grid(row = 2, column = 3)
 Button(root, text = "*").grid(row = 3, column = 3)
 Button(root, text = "-").grid(row = 4, column = 3)
 Button(root, text = "+").grid(row = 5, column = 3)
 Button(root, text = "0", command = lambda: get_numbers(0)).grid(row = 5, column = 1)
 Button(root, text = "=").grid(row = 5, column = 2)
-
+Button(root, text = "DEL", command = lambda: one_by_one()).grid(row = 2, column = 4)
 
 root.mainloop()
